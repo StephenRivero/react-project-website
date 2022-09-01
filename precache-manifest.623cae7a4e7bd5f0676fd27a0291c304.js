cd530cefbf45cae2e4e7b1ffac52ef7b7022ca91
Stephen Manasseh Rivero Pastor <stephen.m.rivero@gmail.com>
2022-09-01 20:14:49 +0800

    Updates

---
+++ precache-manifest.623cae7a4e7bd5f0676fd27a0291c304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a0ba01dbb37ccfbe741",
+    "url": "/react-project-website/static/css/main.7bcf0343.chunk.css"
+  },
+  {
+    "revision": "7a0ba01dbb37ccfbe741",
+    "url": "/react-project-website/static/js/main.c3b6f469.chunk.js"
+  },
+  {
+    "revision": "e8d2cc4aea878ac845cb",
+    "url": "/react-project-website/static/js/runtime~main.1a76d2e6.js"
+  },
+  {
+    "revision": "ab70f77b57756b8d1c48",
+    "url": "/react-project-website/static/js/2.86d5e624.chunk.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/react-project-website/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "8d8bd6902849b6a07acb4bb85af8e869",
+    "url": "/react-project-website/index.html"
+  }
+];
